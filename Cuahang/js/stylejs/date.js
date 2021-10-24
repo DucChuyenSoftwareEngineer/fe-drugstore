8a5414f0b9950037a909b01154973f4e887b229a
ManhDevFrontEnd <81523323+ManhDevFrontEnd@users.noreply.github.com>
2021-10-24 10:35:54 +0700

    lam lai trang chu cua hang, to chuc cau truc thu muc(hoi chuoi do cai win quen git)

--- Cuahang/js/stylejs/date.js
+++
@@ -1,8 +0,0 @@
-const date = document.getElementById("date")
-function showdates(){
-    let day =["Thứ Hai","Thứ Ba","Thứ Tư","Thứ Năm","Thứ Sáu","Thứ 7","Chủ Nhật"]
-    let showdate = new Date();
-    date.innerHTML=day[showdate.getDay()]+" Ngày " + showdate.getDate() + " Tháng "+ (showdate.getMonth()+1) + " Năm "  +showdate.getFullYear()
-   
-}
-showdates()
--- Cuahang/js/stylejs/style.js
+++
@@ -1,8 +0,0 @@
-const escapes = document.getElementById("escape")
-const error = document.getElementById("offerror")
-function turNoff(){
-    escapes.addEventListener("click",function(){
-          error.classList.add("hide")
-    })
-}
-turNoff()
